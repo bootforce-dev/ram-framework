7eba2495e69af1343db764003d31287e55df0eb9
Roman Valov <roman.valov@gmail.com>
2019-06-10 17:05:01 +0000

    add: support for lib install via pip

--- setup.py
+++ setup.py
@@ -4,8 +4,54 @@ from setuptools import setup, find_packages
 
 from pbr.version import VersionInfo
 
+try:
+    from setuptools.command.install_data import install_data
+except ImportError:
+    from distutils.command.install_data import install_data
+
+from setuptools.command.easy_install import is_python_script, get_script_header
+
+import os
+
+
 __project__ = 'ram-framework'
 __version__ = VersionInfo(__project__).release_string()
+
+
+def list_units(install, dirroot):
+    for dirpath, dirs, files in os.walk(dirroot):
+        filelst = [os.path.join(dirpath, _) for _ in files]
+        if filelst:
+            yield (os.path.join(install, dirpath), filelst)
+
+class _install_data(install_data):
+    def run(self):
+        install_data.run(self)
+        for _file in self.outfiles:
+            if not os.path.isfile(_file):
+                continue
+
+            if not os.access(_file, os.X_OK):
+                continue
+
+            with open(_file, 'r') as _:
+                _exec = _.readline()
+                _text = _.read()
+
+            if not _exec.startswith('#!'):
+                continue
+
+            try:
+                compile(_text, _file, 'exec')
+            except SyntaxError:
+                continue
+
+            _exec = get_script_header(_exec)
+
+            with open(_file, 'w') as _:
+                _.write(_exec)
+                _.write(_text)
+
 
 if __name__ == '__main__':
     setup(
@@ -23,7 +69,7 @@ if __name__ == '__main__':
         data_files=[
             ('share/ram', ['share/ram/srv.functions', 'share/ram/ram.functions']),
             ('/etc/bash_completion.d', ['etc/bash_completion.d/ram']),
-        ],
+        ] + list(list_units('', 'lib')),
         scripts=['bin/ram', 'bin/ram-symbols'],
         classifiers=(
             'Development Status :: 3 - Alpha',
